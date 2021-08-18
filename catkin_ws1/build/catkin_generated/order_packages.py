438d9f05973674e288d8b6e0df815f4d8d510343
naoessf <naoessf@gmail.com>
2021-08-18 15:43:45 +0000

    init git

---
+++ catkin_ws1/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/ROS_Test/catkin_ws1/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/ROS_Test/catkin_ws1/devel;/opt/ros/melodic'.split(';') if '/home/ubuntu/ROS_Test/catkin_ws1/devel;/opt/ros/melodic' != '' else []
---
+++ catkin_ws1/build/test1_pkg/CMakeFiles/hello_world_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world_node.dir/src/hello_world_node.cpp.o"
+  "/home/ubuntu/ROS_Test/catkin_ws1/devel/lib/test1_pkg/hello_world_node.pdb"
+  "/home/ubuntu/ROS_Test/catkin_ws1/devel/lib/test1_pkg/hello_world_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
