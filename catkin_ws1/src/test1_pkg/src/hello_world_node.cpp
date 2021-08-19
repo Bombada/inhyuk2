0c84c3fceff928069e324de8564c5a56a84a7483
Bombada <37093814+Bombada@users.noreply.github.com>
2021-08-19 23:23:28 +0900

    Update hello_world_node.cpp
    Using NameSpace

--- catkin_ws1/src/test1_pkg/src/hello_world_node.cpp
+++ catkin_ws1/src/test1_pkg/src/hello_world_node.cpp
@@ -2,26 +2,27 @@
 #include <std_msgs/String.h>
 
 #include <sstream>
-
+using namespace ros;
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "hello_world_node");
-  ros::NodeHandle nh;
-  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("say_hello_world", 1000);
+  init(argc, argv, "hello_world_node"); // ROS를 처음 초기화 함
+  NodeHandle nh; // 뭔 핸들러가 있음
+  Publisher chatter_pub = nh.advertise<std_msgs::String>("say_hello_world", 1000); // 
 
-  ros::Rate loop_rate(10);
-  int count = 0;
-  while (ros::ok())
+  Rate loop_rate(10); 
+  
+  int count = 0;// 카운트
+  while (ok())// ros가 OK인 동안 괄호 안 반복
   {
     std_msgs::String msg;
     std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-    ROS_INFO("%s", msg.data.c_str());
-    chatter_pub.publish(msg);
-    ros::spinOnce();
-    loop_rate.sleep();
-    ++count;
+    ss << "hello world " << count; // ex) hello world 카운트(몇번 반복했는지 보기 위해) 메시지에 담을 준비
+    msg.data = ss.str(); //  메시지 데이터에 실음
+    ROS_INFO("%s", msg.data.c_str()); // ROS 로그에다가 한번 슥 출력
+    chatter_pub.publish(msg);//"hello world 1" 퍼블리셔에다가 전송
+    spinOnce();// 로본 한바퀴 돌아라
+    loop_rate.sleep();// 10만큼 기다림
+    ++count;// 카운트 증가
   }
   return 0;
 }
